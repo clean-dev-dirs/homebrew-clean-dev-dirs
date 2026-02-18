7497ead76b0d4e6c8bb404a7e61e9b1973c09e09
Tom Planche <tomplanche@proton.me>
2026-02-18 01:12:15 +0100

    [2] (feat on main) Add bottle for cleaner stats

--- Formula/clean-dev-dirs.rb
+++ Formula/clean-dev-dirs.rb
@@ -6,6 +6,13 @@ class CleanDevDirs < Formula
   license "Apache-2.0 OR MIT"
   version "2.5.0"
 
+  bottle do
+    root_url "https://github.com/clean-dev-dirs/clean-dev-dirs/releases/download/v2.5.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "PLACEHOLDER_ARM64_SEQUOIA"
+    sha256 cellar: :any_skip_relocation, sequoia:       "PLACEHOLDER_SEQUOIA"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "PLACEHOLDER_X86_64_LINUX"
+  end
+
   depends_on "rust" => :build
 
   def install
